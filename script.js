9291425a77d4e3f0a459aaa18a9763e9f5768a2e
Capi7977 <129705834+Capi7977@users.noreply.github.com>
2024-03-09 00:15:47 -0700

    I manage to figure out hoe to set everything on a table. THe pnly thing that I was not able to figure out is how to use the users input

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const userinput = prompt (
+  "Please enter a list of comma-separated froyo flavors.",
+  "Vanilla,Vanilla,Vanilla,Strawberry,Coffee,Coffee"
+);
+
+const froyo = {
+  Vanilla: 3,
+  Straberry: 1,
+  Coffee: 2,
+};
+
+console.table(froyo)
